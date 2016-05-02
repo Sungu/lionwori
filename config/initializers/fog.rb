d107659b6011066a094c4171c1bbed387a07ab84
Sungu Nam <naisz@naver.com>
2016-05-02 14:52:07 +0000

    ignore real change

--- config/initializers/fog.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',                        
-    aws_access_key_id:  'AKIAJUKEMX3XHA7YHEXQ',
-    aws_secret_access_key: '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',                        # required
-    region:                'ap-northeast-2',                  
-  }
-  config.fog_directory  = 'lionwori'                          
-end
