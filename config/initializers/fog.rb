4f2702dd55892e5062d11496a7dbe8ee81800bcd
Sungu Nam <naisz@naver.com>
2016-05-02 14:49:11 +0000

    change in gitignore

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        
-    aws_access_key_id:  'AKIAIU6534D5T3ES7QAA',
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:  'AKIAJUKEMX3XHA7YHEXQ',
+    aws_secret_access_key: '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',                        # required
     region:                'ap-northeast-2',                  
   }
   config.fog_directory  = 'lionwori'                          
